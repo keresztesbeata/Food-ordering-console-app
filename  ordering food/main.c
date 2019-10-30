24ff010af1b369aec6631eab3cdabdf37a834739
keresztesbeata <keresztesbeata00@yahoo.com>
2019-10-30 20:36:42 +0200

    welcome message

---  ordering food/main.c
+++  ordering food/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    printf("Welcome to Food thingies!\n");
+    printf("Welcome to Food thingies!\n Here you can find all your favourite food!\n");
     return 0;
 }
